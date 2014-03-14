38c5358da1cb08a9954cebe63e409a7a33ba735b
Philippe Possemiers <ppossemiers@telenet.be>
2014-03-14 09:40:00 +0100

    Bug fix

--- app/_attachments/scripts/main.js
+++ app/_attachments/scripts/main.js
@@ -323,7 +323,7 @@ $(document).ready(function(){
 	
     window.addEventListener('load', function(e){
 		  window.applicationCache.addEventListener('updateready', function(e){
-		    if (window.applicationCache.status == window.applicationCache.UPDATEREADY){
+		    if(window.applicationCache.status == window.applicationCache.UPDATEREADY){
 		      window.applicationCache.swapCache();
 		      if (confirm('A new version of GGULIVRR is available. Load it?')){
 		        window.location.reload();
